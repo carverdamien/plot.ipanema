93bce8f74512d5d4b6fd23174a78742417df47be
Damien Carver <carverdamien@gmail.com>
2019-04-01 16:16:04 +0200

    where is my time?

--- src/plotly/time_stack.py
+++ src/plotly/time_stack.py
@@ -11,7 +11,11 @@ description="""
 Plot utility:
 Loads a Dataframe and plots a view of the data.
 """
-TIME_STACK = ['fair','ipanema','sched','kernel','user','idle']
+TIME_STACK = []
+# TIME_STACK = ['fair','ipanema']
+TIME_STACK += ['sched','system','user','idle']
+TIME_STACK += ['softirq','irq','nice','steal','guest','guest_nice','iowait']
+TIME_STACK += ['sm_idle']
 
 def parseCmdLine():
     parser = argparse.ArgumentParser(description=description)
@@ -27,6 +31,8 @@ def get_y(df, load, scheduler, time):
     sel = df['load'] == load
     sel = np.logical_and(sel, df['scheduler'] == scheduler)
     return np.mean(df[time][sel])
+    # return np.min(df[time][sel])
+    # return np.array(df[time][sel])[1]
 
 def save(output, df):
     LOADS      = np.unique(df['load'])
@@ -54,28 +60,62 @@ def save(output, df):
 
 def unified_dataframe(df):
     HEADER = ['load','scheduler'] + TIME_STACK
+    if 'kernel' in df.columns:
+        assert len(np.unique(df['kernel']))==1
+        df.drop(columns=['kernel'],inplace=True)
     if 'clients' in df.columns:
         df.rename(columns={'clients':'load'},inplace=True)
     if 'tasks' in df.columns:
         df.rename(columns={'tasks':'load'},inplace=True)
     if 'client_sched' in df.columns or 'engine_sched' in df.columns:
-        assert df['client_sched'] == df['engine_sched']
+        assert np.sum(df['client_sched'] == df['engine_sched']) == len(df)
         df.drop(columns=['client_sched'],inplace=True)
         df.rename(columns={'engine_sched':'scheduler'},inplace=True)
+    # Unit conversion
+    for header in ['user','system','idle','iowait','softirq','irq','nice','steal','guest','guest_nice']:
+        header = "procstat_{}".format(header)
+        # proc/stat is in USER_HZ ie 10**-2sec getconf CLK_TCK)
+        # schedmonitor is in 10**-9sec
+        # 9-2=7
+        df[header] = 10**7 * df[header]
     logging.info('Mean difference between idle_total_ns and procstat_idle is {}.'.format(
         np.mean(df['idle_total_ns'] - df['procstat_idle'])))
-    df.rename(columns={
-            'procstat_user':'user',
-            'procstat_system':'kernel',
-            #'procstat_idle':'idle',
+    for header in ['procstat_idle','idle_total_ns']:
+        logging.info('{} in [{};{}]'.format(header,np.min(df[header]),np.max(df[header])))
+    columns = {
+        'procstat_{}'.format(k):k
+        for k in ['user','system','idle','iowait','softirq','irq','nice','steal','guest','guest_nice']
+        }
+    columns.update({
             'ipanema_total_ns':'ipanema',
             'fair_total_ns':'fair',
             'sched_total_ns':'sched',
-            'idle_total_ns':'idle',
-            },inplace=True)
+            'idle_total_ns':'sm_idle',
+            })
+    df.rename(columns=columns,inplace=True)
     for header in HEADER:
         assert header in df.columns, "{} not in df.columns".format(header)
     df.drop(columns=[header for header in df.columns if header not in HEADER],inplace=True)
+    df.dropna(inplace=True)
+    df['system'] = df['system'] - df['sched']
+    # df['sched'] = df['sched'] - (df['ipanema'] + df['fair'])
+    # cpu sec
+    for header in TIME_STACK:
+        df[header] = df[header] / 160 / 10**9
+    return df
+    # Ratio conversion
+    total = np.zeros(len(df))
+    for header in TIME_STACK:
+        total += df[header]
+    logging.info('Total min:{}, max:{}, mean:{}, std:{}'.format(
+            np.min(total),
+            np.max(total),
+            np.mean(total),
+            np.std(total),
+            )
+                 )
+    for header in TIME_STACK:
+        df[header] = df[header] / total
     return df
     
 def main():
