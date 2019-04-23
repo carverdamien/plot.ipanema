7fa0fc329d98a669c774587bb4a44b08b39d38f4
Damien Carver <carverdamien@gmail.com>
2019-04-23 17:31:38 +0200

    use Pool to process a single tracer.tgz faster

--- src/parse_rqsize.py
+++ src/parse_rqsize.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python3
-import sys, tarfile, parse, logging
+import sys, tarfile, parse, logging, multiprocessing, os
 import numpy as np
 import pandas as pd
 
 def parse_tracer_file(f):
     p = parse.compile("""{clock:d} RQ_SIZE {pid:d} {size:d} {op:d}""")
-    data = [p.parse(l.decode("utf-8")).named for l in f.readlines()]
-    return pd.DataFrame(data)
+    return pd.DataFrame([p.parse(l.decode("utf-8")).named for l in f.readlines()])
 
 def load_tracer_tgz(path):
     data = { 'clock' : [], 'size' : [] }
@@ -23,12 +22,33 @@ def load_tracer_tgz(path):
                 data[k].append(np.array(df[k]))
     return {k:np.array(data[k]) for k in data}
 
+def func(args):
+    path, name = args
+    with tarfile.open(path) as tgz:
+        members = { m.name:m for m in tgz.getmembers() if m.isreg() }
+        return parse_tracer_file(tgz.extractfile(members[name]))
+
+def load_tracer_tgz_in_parallel(path, parallel):
+    p = multiprocessing.Pool(parallel)
+    data = { 'clock' : [], 'size' : [] }
+    with tarfile.open(path) as tgz:
+        members = { m.name:m for m in tgz.getmembers() if m.isreg() }
+        dfs = p.map(func,[[path,'tracer/{}'.format(cpuid)] for cpuid in range(len(members))])
+        for df in dfs:
+            for k in data:
+                data[k].append(np.array(df[k]))
+    return {k:np.array(data[k]) for k in data}
+        
 def main():
     level = logging.ERROR
     logging.basicConfig(format='[%(levelname)s] %(message)s', level=level)
     rqsize_npz = sys.argv[1]
     tracer_tgz = sys.argv[2]
-    data = load_tracer_tgz(tracer_tgz)
+    if 'PARALLEL' in os.environ:
+        parallel = int(os.environ['PARALLEL'])
+        data = load_tracer_tgz_in_parallel(tracer_tgz,parallel)
+    else:
+        data = load_tracer_tgz(tracer_tgz)
     with open(rqsize_npz, 'wb') as f:
         np.savez(f, **data)
 
