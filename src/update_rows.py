ec5616573fe124837c7650a4f061eb2ca2d227ed
Damien Carver <carverdamien@gmail.com>
2019-05-27 14:00:30 +0200

    drop portage dule

--- src/update_rows.py
+++ src/update_rows.py
@@ -1,6 +1,20 @@
 def update_rows(updates, df):
     for op in updates:
-        assert(op["op"] == "cas")
-        sel = df[op["key"]] == op["old"]
-        df[op["key"]][sel] = op["new"]
+        df = __OP__[op["op"]](op, df)
     return df
+
+def cas(op, df):
+    assert(op["op"] == "cas")
+    sel = df[op["key"]] == op["old"]
+    df[op["key"]][sel] = op["new"]
+    return df
+
+def drop(op, df):
+    assert(op["op"] == "drop")
+    sel = df[op["key"]] != op["val"]
+    return df[sel]
+
+__OP__ = {
+    'cas' : cas,
+    'drop' : drop,
+}
