ebb76f1455960ceb5b222cee9b55622f76920864
Damien Carver <carverdamien@gmail.com>
2019-06-05 15:11:41 +0200

    drop kernel [5ac2a058-63c6e485]

--- metric.mk
+++ metric.mk
@@ -1,5 +1,5 @@
 STACK_METRICS=time_stack enQ_stack enQ_wc_stack deQ_stack
-SCHED_DEBUG_METRICS=enQ_no_reason enQ_new enQ_wakeup enQ_wakeup_mig enQ_lb_mig deQ_no_reason deQ_sleep enQ_wc_no_reason enQ_wc_new enQ_wc_wakeup enQ_wc_wakeup_mig enQ_wc_lb_mig deQ_wc_no_reason deQ_wc_sleep
+# SCHED_DEBUG_METRICS=enQ_no_reason enQ_new enQ_wakeup enQ_wakeup_mig enQ_lb_mig deQ_no_reason deQ_sleep enQ_wc_no_reason enQ_wc_new enQ_wc_wakeup enQ_wc_wakeup_mig enQ_wc_lb_mig deQ_wc_no_reason deQ_wc_sleep
 SCHED_MONITOR_METRICS=sched_total_ns idle_total_ns fair_total_ns ipanema_total_ns
 COMMON_METRICS=$(SCHED_DEBUG_METRICS) $(SCHED_MONITOR_METRICS) $(STACK_METRICS)
 define metric
